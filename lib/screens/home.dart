e31ba43b05b89aabe9ef3bb02e05668ce8ddb96d
Anant Singh <122683904+AnantKumarSingh26@users.noreply.github.com>
2025-08-10 21:07:33 +0530

    Home Screen updated

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,9 +6,26 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        
-      ],)
-    );
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text('Home Screen',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Color.fromARGB(255, 36, 32, 65),),textAlign: TextAlign.center,),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/image3.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: const Text(
+            'Welcome to the Home Screen!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),      
+      );
   }
 }
