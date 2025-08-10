987853576773e73c4e59b2ae1ba13cfc346a15f9
Anant Singh <122683904+AnantKumarSingh26@users.noreply.github.com>
2025-08-10 17:55:57 +0530

    landing screen done

--- lib/main.dart
+++ lib/main.dart
@@ -14,28 +14,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50),
           child: Column(
             children: [
+              const Spacer(flex: 2),
               Image.asset('assets/image.png', fit: BoxFit.cover),
               const SizedBox(height: 20),
-              const Text('Welcome to Squiz \n Challenge your mind, expand your knowledge',style: TextStyle(),textAlign: TextAlign.center,),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+              const Text(
+                'Welcome to Squiz, \n Challenge your mind, expand your knowledge',
+                style: TextStyle(fontFamily: 'Poppins', fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30),
+              const Spacer(flex: 3),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                      ),
+                    ),
+                    child: const Text(
+                      'Get Started',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
                   ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 80,
-                    vertical: 10,
-                  ),
-                ),
-                child: const Text(
-                  'Get Started',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ),
             ],
