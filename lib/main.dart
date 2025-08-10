05cbb12ad74fcb754b96407cd6263cc61593ffac
Anant Singh <122683904+AnantKumarSingh26@users.noreply.github.com>
2025-08-10 17:38:02 +0530

    landing Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,47 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Image.asset('assets/image.png', fit: BoxFit.cover),
+              const SizedBox(height: 20),
+              const Text('Welcome to Squiz \n Challenge your mind, expand your knowledge',style: TextStyle(),textAlign: TextAlign.center,),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 80,
+                    vertical: 10,
+                  ),
+                ),
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
