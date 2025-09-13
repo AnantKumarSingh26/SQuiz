2d972eeca86a77ca30959b6a8e76bf27d09967de
Anant Singh <122683904+AnantKumarSingh26@users.noreply.github.com>
2025-09-13 23:36:03 +0530

    Graph widget added

---
+++ lib/widgets/graph.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProgressGraph extends StatelessWidget {
+  const ProgressGraph({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
